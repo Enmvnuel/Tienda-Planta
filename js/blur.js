793197ff04f737491933433f2a5b36affa117254
ysEnmanuel <cristopherariasp@icloud.com>
2025-06-04 14:02:45 -0500

    Update styles in stylenew.css for improved aesthetics and responsiveness; add blur and smooth scroll functionality in JavaScript files.
    Co-authored-by: cabezassebastian <cabezassebastian@users.noreply.github.com>

---
+++ js/blur.js
@@ -0,0 +1,8 @@
+window.addEventListener("blur", () => {
+    document.title = "¡No te vayas! 😢";
+});
+
+window.addEventListener("focus", () => {
+    document.title =
+        "Minimart - Tu tienda en línea, ¡todo lo que necesitas al alcance de un clic!";
+});
